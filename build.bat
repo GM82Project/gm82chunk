e1313b150bfff3549b72c1a9ce113da9076837c8
omicronrex <65692652+omicronrex@users.noreply.github.com>
2025-08-26 10:42:40 -0300

    fix build script...

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-if not exist gm82gex.py curl https://raw.githubusercontent.com/GM82Project/gm82gex/main/gm82gex.py -o 
+if not exist gm82gex.py curl https://raw.githubusercontent.com/GM82Project/gm82gex/main/gm82gex.py -o gm82gex.py
 
 python gm82gex.py gm82chunk.gej
 
